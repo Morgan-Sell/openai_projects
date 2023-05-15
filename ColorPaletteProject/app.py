458ae10db69410c33803638de4bdffe0fc73a66b
Morgan-Sell <morganpsell@gmail.com>
2023-05-14 20:42:33 -0400

    finish color palette generator. start chapter 5 on important API params.

--- ColorPaletteProject/app.py
+++ ColorPaletteProject/app.py
@@ -1,27 +1,51 @@
 import openai
 from flask import Flask, render_template, request
 from dotenv import dotenv_values
+import json
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__, template_folder='templates')
 
-# @app.route("/palette", methods=["POST"])
-# def prompt_to_palette():
-    # OPEN AI COMPLETION CALL
+def get_colors(msg):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes
+    You should generate color palettes that fit, theme, mood or instruction in the prompt.
+
+    The palettes should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
 
 
-    # RETURN LIST OF COLORS
+    Desired format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    A:
+    """
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        model="text-davinci-003",
+        max_tokens=200,
+    )
+
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    query = request.form.get("query")
+    colors = get_colors(query)
+    return {"colors": colors}
 
 @app.route("/")
 def index():
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Give me a funny word: "
-    )
-    return response["choices"][0]["text"]
-    # return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
